f689616afd0d3931dc5b3e1623e764b9a5cc2397
cerebrophase <lawalmuhdbashir@gmail.com>
2023-02-15 11:00:33 -0800

    I added a header file shell.h and a main.c file

---
+++ shell.h
@@ -0,0 +1,21 @@
+#ifndef _SHELL_H_
+#define _SHELL_H_
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <limits.h>
+
+#define BUFSIZE 1024
+#define TOK_BUFSIZE 128
+#define TOK_DELIM " \t\r\n\a"
+
+
+
+#endif
